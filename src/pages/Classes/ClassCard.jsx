00c5dd2eb5182cb6c7adefaffedd8dcf18e3312a
Merajujjaman <asmmerajujjaman@gmail.com>
2023-06-13 21:35:33 +0600

    select class by students done

---
+++ src/pages/Classes/ClassCard.jsx
@@ -0,0 +1,53 @@
+import React, { useContext, useState } from 'react';
+import { FaArrowRight } from 'react-icons/fa';
+import { AuthContext } from '../../Providers/AuthProvider';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
+
+const ClassCard = ({item, refetch}) => {
+    const [axiosSecure] = useAxiosSecure()
+    const {user} = useContext(AuthContext)
+    const [disabled, setDisabled] = useState(false)
+    const {_id, classImage, className, instructorName, instructorEmail, price, availableSeats} = item;
+    const handleAddtoCart = () => {
+        const saveClass ={classId: _id, studentEmail: user?.email, classImage, className, instructorName, instructorEmail, price: parseFloat(price), availableSeats: parseInt(availableSeats)}
+        axiosSecure.post('/student/selectClass', saveClass)
+        .then(res => {
+            if (res.data.insertedId) {
+                setDisabled(true)
+                Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: 'Go to to Dhashboard and enroll',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+                refetch()
+                
+            }
+        })
+        
+    }
+
+    return (
+        <>
+            <div className="card w-full bg-base-100 shadow-xl group">
+                <figure><img src={item?.classImage} alt="photo not found" className=' object-cover max-h-72 w-full group-hover:scale-110 transition-all' /></figure>
+                <div className="card-body">
+                    <h2 className="card-title text-primary">
+                        {item?.className}
+                        <div className="badge badge-secondary">Available</div>
+                    </h2>
+                    <h3 className='font-semibold'>Instructor: {item?.instructorName}</h3>
+                    <p>Available Seats: {item?.availableSeats}</p> 
+                    <p>Price: ${item?.price}</p> 
+                    <div className="card-actions justify-end">
+                        <button disabled={disabled || item?.availableSeats == 0} className='btn btn-primary btn-sm' onClick={handleAddtoCart} >Add to Dashboard <FaArrowRight></FaArrowRight></button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ClassCard;
--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import useAllclasses from '../../hooks/useAllclasses';
 import SectionTitle from '../../Routs/components/SectionTitle';
+import ClassCard from './ClassCard';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
 
 const Classes = () => {
-    const [allClasses] = useAllclasses()
+    // const [allClasses] = useAllclasses()
+    const [axiosSecure] = useAxiosSecure()
+    const {data: classes=[], refetch} = useQuery({
+        queryKey:['classes'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/student/classes')
+            return res.data
+        }
+    })
+
+    console.log(classes);
+
     return (
         <div>
             <SectionTitle title={"Select Your Classes"}></SectionTitle>
-            
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+                {
+                    classes.map(item => <ClassCard
+                    key={item._id}
+                    item={item}
+                    refetch={refetch}
+                    ></ClassCard>)
+                }
+            </div>
         </div>
     );
 };
